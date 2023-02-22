c5247a07b7ddc4976e4958db27be50760566eda6
Anatan <zyabkinantonv@gmail.com>
2023-02-22 19:43:44 +0300

    initPods

---
+++ WebAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebAdventure
+//
+//  Created by Anton Zyabkin on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
